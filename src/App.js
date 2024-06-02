5ccebe7c1392f965f11399df16ab2398cf57dd5e
70ni <4thedevelopments@gmail.com>
2024-06-02 18:39:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { axisClasses } from "@mui/x-charts/ChartsAxis";
+import "./chart.css";
+import CurrentGraph from "./Components/Graph/CurrentGraph";
 
-function App() {
+function TickParamsSelector({
+  tickPlacement,
+  tickLabelPlacement,
+  setTickPlacement,
+  setTickLabelPlacement,
+}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Stack
+      direction="column"
+      justifyContent="space-between"
+      sx={{ width: "100%" }}
+    >
+      <FormControl>
+        <FormLabel id="tick-placement-radio-buttons-group-label">
+          tickPlacement
+        </FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="tick-placement-radio-buttons-group-label"
+          name="tick-placement"
+          value={tickPlacement}
+          onChange={(event) => setTickPlacement(event.target.value)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FormControlLabel value="start" control={<Radio />} label="start" />
+          <FormControlLabel value="end" control={<Radio />} label="end" />
+          <FormControlLabel value="middle" control={<Radio />} label="middle" />
+          <FormControlLabel
+            value="extremities"
+            control={<Radio />}
+            label="extremities"
+          />
+        </RadioGroup>
+      </FormControl>
+      <FormControl>
+        <FormLabel id="label-placement-radio-buttons-group-label">
+          tickLabelPlacement
+        </FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="label-placement-radio-buttons-group-label"
+          name="label-placement"
+          value={tickLabelPlacement}
+          onChange={(event) => setTickLabelPlacement(event.target.value)}
+        >
+          <FormControlLabel value="tick" control={<Radio />} label="tick" />
+          <FormControlLabel value="middle" control={<Radio />} label="middle" />
+        </RadioGroup>
+      </FormControl>
+    </Stack>
   );
 }
 
-export default App;
+const dataset = [
+  {
+    value: 28,
+    currency: "USD",
+  },
+
+  {
+    value: 28,
+    currency: "fpk",
+  },
+  {
+    value: 8,
+    currency: "FRP",
+  },
+  {
+    value: 20,
+    currency: "LLM",
+  },
+  {
+    value: 42,
+    currency: "D",
+  },
+  {
+    value: 2,
+    currency: "MLB",
+  },
+  {
+    value: 42,
+    currency: "PRM",
+  },
+];
+
+const valueFormatter = (value) => `${value}`;
+
+const chartSetting = {
+  yAxis: [
+    {
+      label: `rate in ${dataset.currency}`,
+    },
+  ],
+  series: [
+    { dataKey: "value", label: `rate in ${dataset.currency}`, valueFormatter },
+  ],
+  height: 300,
+  sx: {
+    [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+      transform: "translateX(-10px)",
+    },
+  },
+};
+
+export default function TickPlacementBars() {
+  const [tickPlacement, setTickPlacement] = React.useState("middle");
+  const [tickLabelPlacement, setTickLabelPlacement] = React.useState("middle");
+
+  return (
+    <div style={{ width: "50%" }}>
+      <CurrentGraph />
+      <BarChart
+        dataset={dataset}
+        borderRadius={4}
+        grid={{ vertical: false }}
+        leftAxis={null}
+        xAxis={[
+          {
+            scaleType: "band",
+            dataKey: "currency",
+            //underline value here ........s
+            tickPlacement,
+          },
+        ]}
+        {...chartSetting}
+      />
+    </div>
+  );
+}
