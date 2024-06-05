11a0561950059c9a28340cdfc95d30d97004a552
70ni <4thedevelopments@gmail.com>
2024-06-05 17:11:27 +0530

    Added chart js, remove unused Map section from App.js

--- src/App.js
+++ src/App.js
@@ -10,57 +10,13 @@ import { axisClasses } from "@mui/x-charts/ChartsAxis";
 import CurrentGraph from "./Components/Graph/CurrentGraph";
 import Flactuation from "./Components/Graph/Flactuation";
 import "../src/Components/Graph/style.css";
+import Menu from "./Components/Menu/Menu";
 function TickParamsSelector({
   tickPlacement,
   tickLabelPlacement,
   setTickPlacement,
   setTickLabelPlacement,
-}) {
-  return (
-    <Stack
-      direction="column"
-      justifyContent="space-between"
-      sx={{ width: "100%" }}
-    >
-      <FormControl>
-        <FormLabel id="tick-placement-radio-buttons-group-label">
-          tickPlacement
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="tick-placement-radio-buttons-group-label"
-          name="tick-placement"
-          value={tickPlacement}
-          onChange={(event) => setTickPlacement(event.target.value)}
-        >
-          <FormControlLabel value="start" control={<Radio />} label="start" />
-          <FormControlLabel value="end" control={<Radio />} label="end" />
-          <FormControlLabel value="middle" control={<Radio />} label="middle" />
-          <FormControlLabel
-            value="extremities"
-            control={<Radio />}
-            label="extremities"
-          />
-        </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel id="label-placement-radio-buttons-group-label">
-          tickLabelPlacement
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="label-placement-radio-buttons-group-label"
-          name="label-placement"
-          value={tickLabelPlacement}
-          onChange={(event) => setTickLabelPlacement(event.target.value)}
-        >
-          <FormControlLabel value="tick" control={<Radio />} label="tick" />
-          <FormControlLabel value="middle" control={<Radio />} label="middle" />
-        </RadioGroup>
-      </FormControl>
-    </Stack>
-  );
-}
+}) {}
 
 const dataset = [
   {
