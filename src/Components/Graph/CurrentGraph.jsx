32fdc960cab244fddaf82307ecf6a7981ad41030
70ni <4thedevelopments@gmail.com>
2024-06-05 17:08:27 +0530

    Current Graph Updated with Menu and Menu action like adding new graph

--- src/Components/Graph/CurrentGraph.jsx
+++ src/Components/Graph/CurrentGraph.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useRef, useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
@@ -10,6 +10,37 @@ import { axisClasses } from "@mui/x-charts/ChartsAxis";
 import add from "../../Image/Add.svg";
 import "./style.css";
 import HistoricData from "./HistoricData";
+import Menu from "../Menu/Menu";
+
+import { useComponentVisible } from "../../Functions/useComponentVisible";
+// export function useComponentVisible(initialIsVisible) {
+//   const [isComponentVisible, setIsComponentVisible] =
+//     useState(initialIsVisible);
+//   const ref = useRef(null);
+
+//   const handleHideDropdown = (event: KeyboardEvent) => {
+//     if (event.key === "Escape") {
+//       setIsComponentVisible(false);
+//     }
+//   };
+
+//   const handleClickOutside = (event) => {
+//     if (ref.current && !ref.current.contains(event.target)) {
+//       setIsComponentVisible(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener("keydown", handleHideDropdown, true);
+//     document.addEventListener("click", handleClickOutside, true);
+//     return () => {
+//       document.removeEventListener("keydown", handleHideDropdown, true);
+//       document.removeEventListener("click", handleClickOutside, true);
+//     };
+//   });
+
+//   return { ref, isComponentVisible, setIsComponentVisible };
+// }
 
 const dataset = [
   {
@@ -44,8 +75,14 @@ const dataset = [
   },
 ];
 
-const colo = "#d9d9d9";
+const currencyList = ["INR", "USD", "GPB", "KWD", "ETH"];
 function CurrentGraph() {
+  const [NewCurrency, setNewCurrency] = useState("");
+  console.log(NewCurrency);
+  const newCurrencyUpdate = (currency) => {
+    return setNewCurrency(currency);
+  };
+
   const [color, setcolor] = useState("#d9d9d9");
   const valueFormatter = (value) => `${value}`;
 
@@ -64,9 +101,45 @@ function CurrentGraph() {
     },
   };
 
+  // const handleClickOutside = (event) => {
+  //   if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+  //     setvis(true);
+  //     console.log("Hello there");
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", handleClickOutside());
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutside());
+  //   };
+  // }, []);
+
+  ////////////////////////////////////////////
+  const NewGraph = {
+    value: 12,
+    currency: NewCurrency,
+  };
+
+  function NewGraphUpdate() {
+    dataset.push(NewGraph);
+    // dataset.shift();
+    const index = dataset[1];
+    dataset.splice(1, 1);
+    console.log(dataset.length);
+  }
+
+  useEffect(() => {
+    return NewGraphUpdate();
+  }, [NewCurrency]);
+
+  const { ref, isComponentVisible, setIsComponentVisible } =
+    useComponentVisible(false);
+
   return (
     <div className="current-wrapper">
       <div className="Header-section">
+   
         <div className="cur-left">
           <div className="Header">Currency Graph</div>
           <div className="para">
@@ -110,7 +183,20 @@ function CurrentGraph() {
                 ]}
                 {...chartSetting}
               />
-              <img src={add} alt="" className="addIcon" />
+              <div className="menu-section">
+                <img
+                  src={add}
+                  alt=""
+                  className="addIcon"
+                  onClick={() => setIsComponentVisible(true)}
+                />
+                <Menu
+                  ref={ref}
+                  hidden={isComponentVisible}
+                  newCurrencyUpdate={newCurrencyUpdate}
+                  currencyList={currencyList}
+                />
+              </div>
             </div>
           </div>
           <HistoricData />
