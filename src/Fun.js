cbf676b643ef7d55a3df32f9c5dbd8b906d5e1cb
70ni <4thedevelopments@gmail.com>
2024-06-27 15:51:42 +0530

    minor updates

--- src/Fun.js
+++ src/Fun.js
@@ -1,6 +1,18 @@
 import data from "../src/Components/Api/eur.json";
 
-const curr = ["kwd", "inr", "usd", "zen", "zypto"];
+const curr = [
+  "usd",
+  "eur",
+  "gbp",
+  "cny",
+  "aud",
+  "cad",
+  "chf",
+  "hkd",
+  "sgd",
+  "aed",
+];
+const obj = [];
 
 export const fire = () => {
   //   let currency = data.eur.key;
@@ -8,41 +20,39 @@ export const fire = () => {
   //   return console.log(nameKey);
 
   //   const a = Object.keys(data.eur).filter((x, y) => x === "inr");
-  const a = Object.entries(data.eur);
-  //   return console.log(a);
+  //   const a = Object.entries(data.eur);
+  //   //   return console.log(a);
 
-  //   curr.map((x) => console.log(x));
-  for (const key in data.eur) {
-    // let c = key == curr.map((x) => x.toString());
-    // console.log(key);
-    // console.log(c);
-  }
-  const obj = [];
+  //   //   curr.map((x) => console.log(x));
+  //   for (const key in data.eur) {
+  //     // let c = key == curr.map((x) => x.toString());
+  //     // console.log(key);
+  //     // console.log(c);
+  //   }
 
   let indexOfcurrency;
   for (let i = 0; i < curr.length; i++) {
-    console.log(curr[i]);
+    // console.log(curr[i]);
     indexOfcurrency = Object.keys(data.eur).indexOf(curr[i]);
-    console.log(indexOfcurrency);
-
+    // console.log(indexOfcurrency);
     let value = Object.entries(data.eur);
-    console.log(value[indexOfcurrency]);
+    // console.log(value[indexOfcurrency]);
 
     const dap = value[indexOfcurrency];
     console.log(dap);
 
     let name = dap[0];
     let valu = dap[1];
-    obj.push({ name: name, value: valu });
-    // dap.map((value) =>  (
-    //     {...obj,
-    //         obj.name
-    //     }
-
-    // );
-    // obj.push((obj.name = dap[0]), (obj.currency = dap[1]));
-    console.log(obj);
+    obj.push({ currency: name, value: valu });
   }
+  // dap.map((value) =>  (
+  //     {...obj,
+  //         obj.name
+  //     }
+
+  // );
+  // obj.push((obj.name = dap[0]), (obj.currency = dap[1]));
+  console.log(obj);
 
   //   const obj = [{}];
   //   arr.map((value) => ((obj.name = dap[0]), (obj.cur = dap[1])));
@@ -55,3 +65,5 @@ export const fire = () => {
   //   return console.log(a);
   // }
 };
+
+export default obj;
