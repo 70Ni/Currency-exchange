3e60c1754c54ba589afc2267c6beaa4d14fc7c0b
70ni <4thedevelopments@gmail.com>
2024-06-27 13:44:29 +0530

    function for summarizing the data for graph

---
+++ src/Fun.js
@@ -0,0 +1,57 @@
+import data from "../src/Components/Api/eur.json";
+
+const curr = ["kwd", "inr", "usd", "zen", "zypto"];
+
+export const fire = () => {
+  //   let currency = data.eur.key;
+  //   const nameKey = Object.keys(data.eur).find((x) => x == "KWD" && "INR");
+  //   return console.log(nameKey);
+
+  //   const a = Object.keys(data.eur).filter((x, y) => x === "inr");
+  const a = Object.entries(data.eur);
+  //   return console.log(a);
+
+  //   curr.map((x) => console.log(x));
+  for (const key in data.eur) {
+    // let c = key == curr.map((x) => x.toString());
+    // console.log(key);
+    // console.log(c);
+  }
+  const obj = [];
+
+  let indexOfcurrency;
+  for (let i = 0; i < curr.length; i++) {
+    console.log(curr[i]);
+    indexOfcurrency = Object.keys(data.eur).indexOf(curr[i]);
+    console.log(indexOfcurrency);
+
+    let value = Object.entries(data.eur);
+    console.log(value[indexOfcurrency]);
+
+    const dap = value[indexOfcurrency];
+    console.log(dap);
+
+    let name = dap[0];
+    let valu = dap[1];
+    obj.push({ name: name, value: valu });
+    // dap.map((value) =>  (
+    //     {...obj,
+    //         obj.name
+    //     }
+
+    // );
+    // obj.push((obj.name = dap[0]), (obj.currency = dap[1]));
+    console.log(obj);
+  }
+
+  //   const obj = [{}];
+  //   arr.map((value) => ((obj.name = dap[0]), (obj.cur = dap[1])));
+  //   arr.map((value) => obj.push((obj.name = dap[0]), (obj.cur = dap[1])));
+
+  //   console.log(obj);
+
+  // for (let i = 0; i >= curr.length; i++) {
+  //   let a = data.eur.map((currency) => currency === i);
+  //   return console.log(a);
+  // }
+};
