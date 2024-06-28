33c217dfb7085b855281f2ee76d060123bbfe22c
70ni <4thedevelopments@gmail.com>
2024-06-28 15:08:42 +0530

    Added date range and details

---
+++ src/Components/StatusBar/DateSection.jsx
@@ -0,0 +1,22 @@
+import Button from "../../Components/Menu/Button";
+import "./status.css";
+function DateSection() {
+  return (
+    <div className="date-range-wrapper">
+      <div className="current-date">30 May, 4:53 am UTC</div>
+      <div className="status-range-selector">
+        <div className="st-date-buttons" style={{ marginRight: 20 }}>
+          from
+          <div className="tag">12-12-24</div>
+        </div>
+        <div className="st-date-buttons">
+          to
+          <div className="tag">12-12-24</div>
+        </div>
+      </div>
+      {/* <Button buttonText={"Go live"} /> */}
+    </div>
+  );
+}
+
+export default DateSection;
