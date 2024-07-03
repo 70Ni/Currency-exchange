2d4adb9a62d31c8cfcd88988be32f7573d6fae22
70ni <4thedevelopments@gmail.com>
2024-07-03 20:42:23 +0530

    clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,82 +1,55 @@
 import * as React from "react";
-import Stack from "@mui/material/Stack";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Radio from "@mui/material/Radio";
-import { BarChart } from "@mui/x-charts/BarChart";
+
 import { axisClasses } from "@mui/x-charts/ChartsAxis";
 import CurrentGraph from "./Components/Graph/CurrentGraph";
 import Flactuation from "./Components/Graph/Flactuation";
+import DateSection from "./Components/StatusBar/DateSection";
 import "../src/Components/Graph/style.css";
-import Menu from "./Components/Menu/Menu";
-function TickParamsSelector({
-  tickPlacement,
-  tickLabelPlacement,
-  setTickPlacement,
-  setTickLabelPlacement,
-}) {}
+import { fire } from "./dataFilter";
+import StatusBar from "./Components/StatusBar/StatusBar";
+import "./App.css";
 
-const dataset = [
-  {
-    value: 28,
-    currency: "USD",
-  },
+// useEffect(() => {
+//   // Function to fetch data
+//   const fetchData = async () => {
+//     try {
+//       // Fetch data from the API
+//       const response = await fetch(
+//         `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.5.30/v1/country.json`
+//       );
+//       // Check if request was successful
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch data");
+//       }
+//       // Parse JSON response
+//       const jsonData = await response.json();
+//       // Update state with fetched data
+//       setweatherData(jsonData);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
+//   // Call fetchData function when component mounts
+//   fetchData();
+//   // Clean up function (optional)
+//   return () => {
+//     // Any cleanup code goes here
+//     // 2.uncomment this <==================================
+//     // setweatherData(details);
+//   };
+// }, []);
 
-  {
-    value: 28,
-    currency: "fpk",
-  },
-  {
-    value: 8,
-    currency: "FRP",
-  },
-  {
-    value: 20,
-    currency: "LLM",
-  },
-  {
-    value: 42,
-    currency: "D",
-  },
-  {
-    value: 2,
-    currency: "MLB",
-  },
-  {
-    value: 42,
-    currency: "PRM",
-  },
-];
-
-const valueFormatter = (value) => `${value}`;
-
-const chartSetting = {
-  yAxis: [
-    {
-      label: `rate in ${dataset.currency}`,
-    },
-  ],
-  series: [
-    { dataKey: "value", label: `rate in ${dataset.currency}`, valueFormatter },
-  ],
-  height: 300,
-  sx: {
-    [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-      transform: "translateX(-10px)",
-    },
-  },
-};
-
-export default function TickPlacementBars() {
-  const [tickPlacement, setTickPlacement] = React.useState("middle");
-  const [tickLabelPlacement, setTickLabelPlacement] = React.useState("middle");
-
+export default function Apps() {
   return (
-    <div className="dashboard-wrapper">
-      <CurrentGraph />
-      <Flactuation />
+    <div className="App">
+      <div className="dashboard-wrapper">
+        <StatusBar />
+        <DateSection />
+        <div className="dashboard-graph-wrapper">
+          <CurrentGraph />
+          <Flactuation />
+        </div>
+      </div>
     </div>
   );
 }
